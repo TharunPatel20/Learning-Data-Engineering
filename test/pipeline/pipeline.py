cbeeabc85c14fff9e46ed64388c8d90cf5896880
Tharun <106445211+TharunPatel20@users.noreply.github.com>
2026-01-19 12:44:01 +0000

    pipeline.py

--- test/pipeline/pipeline.py
+++ test/pipeline/pipeline.py
@@ -4,9 +4,10 @@ import pandas as pd
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
 
-print('arguements', sys.argv)
+# print('arguements', sys.argv)
 month = int(sys.argv[1])
 df = pd.DataFrame({'a': [1, 2], 'b': [3, 4]})
 
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
 
 print(f"Hello pipeline, month:{month}")
